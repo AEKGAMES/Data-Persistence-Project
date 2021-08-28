61f158c3f4b9e714e3f26ab0bb74c08c2d3b69b7
AEK GAMES <anas811@gmail.com>
2021-08-28 05:21:02 +0100

    initial commit
    Start Breakout Game  Imported

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuUIHandler : MonoBehaviour
+{
+    [SerializeField] private Text welcomeText;
+    [SerializeField] private Text nameInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        welcomeText.text = "Hi " + GameManager.Instatnce.PlayerName + ":)";
+    }
+
+    public void SetPlayerName()
+    {
+        welcomeText.text = "*" + nameInput.text + "*";
+        GameManager.Instatnce.SetPlayerName(nameInput.text);
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene("main");
+    }
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+        GameManager.Instatnce.SaveHighScores();
+    }
+    public void LoadSettingsScreen()
+    {
+        SceneManager.LoadScene("SettingsScreen");
+    }
+}
